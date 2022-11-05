3f180d078617cb53889e821ebf4fb0ba8fc43b2c
Remy <remy.sainsard@gmail.com>
2022-11-05 22:39:19 +0100

    Redamon asset with player movement to all cardinal points

---
+++ Assets/PlayerMovementV2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementV2 : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    [SerializeField] private float moveSpeed = 1.0f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPos = rigidbody.position;
+
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector2 inputVector = new Vector2(moveH, moveV);
+
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * moveSpeed;
+
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rigidbody.MovePosition(newPos);
+
+        FindObjectOfType<PlayerAnimation>().SetDirection(inputVector);
+    }
+}
--- Assets/Resources/export.sh
+++ Assets/Resources/export.sh
@@ -15,8 +15,31 @@ fi
 
 ### END ISOMETRIC TILES ###
 
+### DECORATIONS ###
+"$aseprite" -b Decorations/column_basement.aseprite --sheet Decorations/column_basement.png
+"$aseprite" -b Decorations/torch.aseprite --sheet Decorations/torch.png
+"$aseprite" -b Decorations/pixel_flame.aseprite --sheet Decorations/pixel_flame.png
+### END DECORATIONS ###
+
 ### PLAYER ###
 "$aseprite" -b Player/idle_player.aseprite --sheet Player/idle_player.png
 "$aseprite" -b Player/run_player.aseprite --sheet Player/run_player.png
+### REDAMON ###
+"$aseprite" -b Player/Redamon/idle_N_player.aseprite --sheet Player/Redamon/idle_N_player.png
+"$aseprite" -b Player/Redamon/idle_NW_player.aseprite --sheet Player/Redamon/idle_NW_player.png
+"$aseprite" -b Player/Redamon/idle_NE_player.aseprite --sheet Player/Redamon/idle_NE_player.png
+"$aseprite" -b Player/Redamon/idle_W_player.aseprite --sheet Player/Redamon/idle_W_player.png
+"$aseprite" -b Player/Redamon/idle_E_player.aseprite --sheet Player/Redamon/idle_E_player.png
+"$aseprite" -b Player/Redamon/idle_SW_player.aseprite --sheet Player/Redamon/idle_SW_player.png
+"$aseprite" -b Player/Redamon/idle_SE_player.aseprite --sheet Player/Redamon/idle_SE_player.png
+"$aseprite" -b Player/Redamon/idle_S_player.aseprite --sheet Player/Redamon/idle_S_player.png
+"$aseprite" -b Player/Redamon/run_N_player.aseprite --sheet Player/Redamon/run_N_player.png
+"$aseprite" -b Player/Redamon/run_NW_player.aseprite --sheet Player/Redamon/run_NW_player.png
+"$aseprite" -b Player/Redamon/run_NE_player.aseprite --sheet Player/Redamon/run_NE_player.png
+"$aseprite" -b Player/Redamon/run_W_player.aseprite --sheet Player/Redamon/run_W_player.png
+"$aseprite" -b Player/Redamon/run_E_player.aseprite --sheet Player/Redamon/run_E_player.png
+"$aseprite" -b Player/Redamon/run_SW_player.aseprite --sheet Player/Redamon/run_SW_player.png
+"$aseprite" -b Player/Redamon/run_SE_player.aseprite --sheet Player/Redamon/run_SE_player.png
+"$aseprite" -b Player/Redamon/run_S_player.aseprite --sheet Player/Redamon/run_S_player.png
 ### END PLAYER ###
 
